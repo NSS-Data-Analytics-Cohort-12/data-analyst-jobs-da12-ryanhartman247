2e2f39cb7445e4cd7c0b067181712374cb5fe652
Ryan Hartman <you@example.com>
2024-06-04 20:02:13 -0500

    question_9

--- scripts/all_questions.sql
+++ scripts/all_questions.sql
@@ -26,14 +26,42 @@
 -- Answer: 3
 
 -- 5.	How many postings in the dataset have a review count between 500 and 1000?
+-- SELECT *
+-- FROM data_analyst_jobs 
+-- WHERE review_count BETWEEN 500 AND 1000
+-- Answer: 151
 
 -- 6.	Show the average star rating for companies in each state. The output should show the state as `state` and the average rating for the state as `avg_rating`. Which state shows the highest average rating?
+-- SELECT 
+-- 	location AS state, 
+-- 	AVG(star_rating) AS avg_rating
+-- FROM data_analyst_jobs
+-- GROUP BY 1 
+-- ORDER BY 2 desc
 
 -- 7.	Select unique job titles from the data_analyst_jobs table. How many are there?
+-- SELECT COUNT(DISTINCT(title))
+-- FROM data_analyst_jobs
+-- Answer: 881
 
 -- 8.	How many unique job titles are there for California companies?
+-- SELECT 
+-- 	DISTINCT title,
+-- 	location
+-- FROM data_analyst_jobs
+-- WHERE location = 'CA'
+-- Answer: 230
 
 -- 9.	Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
+-- SELECT 
+-- 	company,
+-- 	review_count,
+-- 	AVG(star_rating)
+-- FROM data_analyst_jobs
+-- GROUP BY company, review_count
+-- HAVING review_count > 5000
+-- ORDER BY 3 desc
+-- Answer: 46
 
 -- 10.	Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
 
